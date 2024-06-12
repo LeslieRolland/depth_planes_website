7e9765c1903c2d333b8db0a81afb7302392ee2bf
Jean-baptiste Olivier <jean-baptiste@olivier.contact>
2024-06-12 16:02:27 +0200

    Add : API call . Step 1 + step 2 : ok.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import requests
 from streamlit_image_comparison import image_comparison
+import streamlit.components.v1 as components
 from PIL import Image
 import tempfile
 import time
+import io
 
+url_api = "http://127.0.0.1:8000/"
 
 st.set_page_config(
     page_title="Depth Planes", # => Quick reference - Streamlit
@@ -68,6 +71,11 @@ if img_file:
 
     compute_button = button_placeholder.button("Compute depth map", key='button1')
 
+    # API call for the depth map
+    img_data = img_file.getvalue() # To read file as bytes
+    response = requests.post(url=f'{url_api}depthmap?',files={'file':img_data})
+    response_img = Image.open(io.BytesIO(response.content))
+
     if compute_button: ### Make depth prediction with API
         # Nettoyer les placeholders
         button_placeholder.empty()
@@ -78,8 +86,8 @@ if img_file:
             time.sleep(5)
 
         # Chargement de l'image de comparaison --> ici le call API
-        image = Image.open('/Users/leslierolland/code/soapoperator/depth_planes_website/Examples-of-depth-image.png')
-        path_img2=save_temp_image(image)
+        image_depth = response_img
+        path_img2 = save_temp_image(image_depth)
 
         # Nettoyer les placeholders
         file_uploader_placeholder.empty()
@@ -87,67 +95,9 @@ if img_file:
         image_display_placeholder.empty()
 
         # Display original image VS depth image
-        image_comparison(img1=path_img1, img2=path_img2, label1="Original Image", label2="Depth map")
+        if path_img1 and path_img2:
+            image_comparison(img1=path_img1, img2=path_img2, label1="Original Image", label2="Depth map")
 
 
 
 params = {}
-
-    #depth_planes_api_url = ''
-    #response = requests.get(depth_planes_api_url, params=params)
-    # prediction = response.json()
-    # pred = prediction['url']
-
-
-### Display all the planes
-
-### HTML et CSS pour créer un effet de parallaxe avec plusieurs couches
-# st.markdown("""
-# <style>
-# .parallax {
-#   position: relative;
-#   width: 100%;
-#   height: 500px;
-#   overflow-x: hidden;
-#   overflow-y: hidden;
-# }
-
-# .parallax__layer {
-#   position: absolute;
-#   width: 100%;
-#   height: 100%;
-# }
-
-# /* Ajustez ces valeurs pour créer l'effet désiré */
-# .parallax__layer--back {
-#   transform: translateZ(-2px) scale(2);
-#   z-index: 1;
-# }
-
-# .parallax__layer--middle {
-#   transform: translateZ(-1px) scale(1.5);
-#   z-index: 2;
-# }
-
-# .parallax__layer--front {
-#   transform: translateZ(0) scale(1);
-#   z-index: 3;
-# }
-# </style>
-# """, unsafe_allow_html=True)
-
-# Add your images here dynamically injecting the file paths into the HTML
-# html_content = f"""
-# <div class="parallax">
-#     <div class="parallax__layer parallax__layer--back">
-#         <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2018/08/vacances-ete-montagne-sante-bienfaits.png" alt="Background" style="width:100%;">
-#     </div>
-#     <div class="parallax__layer parallax__layer--middle">
-#         <img src="https://www.allibert-trekking.com/uploads/media/images/thumbnails/AMin3_10-photo-montagne-chine-lac.jpeg" alt="Middle" style="width:100%;">
-#     </div>
-#     <div class="parallax__layer parallax__layer--front">
-#         <img src="https://www.allibert-trekking.com/uploads/media/images/851-jma-montagne.jpeg" alt="Foreground" style="width:100%;">
-#     </div>
-# </div>
-# """
-# st.markdown(html_content, unsafe_allow_html=True)
